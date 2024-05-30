9d07eeea85e15c65d0474ab3e7e6e2739b0a5333
astromanreal <www.astroman6569@gmail.com>
2024-05-30 09:18:40 +0530

    user problem fixing

--- controllers/User.js
+++ controllers/User.js
@@ -40,7 +40,7 @@ exports.register = async (req, res) => {
     res.status(201).json({ message: "User registered successfully" });
   } catch (err) {
     console.error("Server error:", err.message);
-    // res.status(500).json({ message: err.message });
+    res.status(500).json({ message: err.message });
   }
 };
 
